384f48e315f8518cd970884df16d4b48245eb065
Luis Gerardo Barquero Aguilar <LGBARQUEROA@bncrcp.inst.bncr.fi.cr>
2022-02-28 12:22:14 -0600

    Se agrega la automatización de consultarConectividad.js y su archivo xml

---
+++ cypress/integration/convenios/consultarConectividad.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+const convenios = require('../../fixtures/path.json')
+
+
+describe('Convenios', () => {
+    it('Consultar Conectividad', () => {
+
+        let path = convenios.convenios.path
+        
+        cy.fixture('../fixtures/convenios/consultarConectividad.xml').then((body) => {
+            let xmlString = body;
+            let parser = new DOMParser();
+            let xml = parser.parseFromString(xmlString, "application/xml");
+            let convenio = xml.getElementsByTagName("sn:convenio")[0].childNodes[0].nodeValue
+            let empresa = xml.getElementsByTagName("sn:empresa")[0].childNodes[0].nodeValue
+            let tipoLlaveAcceso = xml.getElementsByTagName("sn:tipoLlaveAcceso")[0].childNodes[0].nodeValue
+            let llaveAcceso = xml.getElementsByTagName("sn:llaveAcceso")[0].childNodes[0].nodeValue
+
+            cy.postMethod(path, body).then((response) => {
+                
+                let xmlString = response.body;
+                let parser = new DOMParser();
+                let xml = parser.parseFromString(xmlString, "application/xml");
+                
+                    cy.convertToJson(xml).then((json) =>{
+                        expect(json).not.to.be.empty
+                        expect(json["env:Body"]["sn:respuesta"]["xmlns:sn"]).equals('http://www.bncr.fi.cr/soa/SN_ConectividadConsulta')
+                        expect(json["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:rubros"]["sn:rubro"]["sn:convenio"]).equals(convenio)
+                        expect(json["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:rubros"]["sn:rubro"]["sn:empresa"]).equals(empresa)
+                        expect(json["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:rubros"]["sn:rubro"]["sn:llaveAcceso"]).equals(llaveAcceso)
+                        expect(json["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:rubros"]["sn:rubro"]["sn:tipoLlaveAcceso"]).equals(tipoLlaveAcceso)
+                        expect(json["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:resultado"]["sn:estado"]).equals('00')
+                        expect(json["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:resultado"]["sn:mensaje"]).equals('Transacción completa')
+                    })
+                
+                expect(response.status).eq(200)
+                assert.equal(response.headers['content-type'], 'text/xml; charset=utf-8')
+            })
+        })
+    })
+})
