004049e7d6b213c7123277916dc22a56deb11282
Luis Gerardo Barquero Aguilar <LGBARQUEROA@bncrcp.inst.bncr.fi.cr>
2022-03-17 16:40:53 -0600

    Se agrega nuevas validaciones en consultarLineasCliente.js

--- cypress/integration/prestamos/01-consultarLineasCliente.js
+++ cypress/integration/prestamos/01-consultarLineasCliente.js
@@ -16,11 +16,20 @@ describe('Prestamos', ()  => {
                 
                     cy.convertToJson(xml).then((json) =>{
                         expect(json).not.to.be.empty
+
+                        if (json["soap-env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:datosLineas"]["sn:datosLinea"].length > 1) {
+                            expect(json["soap-env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:datosLineas"]["sn:datosLinea"]).length.above(0)
+                        }
+                        else{
+                            expect(json["soap-env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:datosLineas"]["sn:datosLinea"]).not.to.be.empty
+                        }
+
                         expect(json["soap-env:Body"]["sn:respuesta"]["xmlns:sn"]).equals('http://www.bncr.fi.cr/soa/SN_ConsultaLineas')
                         expect(json["soap-env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:resultado"]["esq:codigo"]).equals('0')
                     })
 
                     expect(response.status).eq(200)
+                    expect(response.body).contains('sn:datosLineas')
                     assert.equal(response.headers['content-type'], 'text/xml; charset=utf-8')
             })
         })
