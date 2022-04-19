861db9a7bbe53d16c34531430eb5910dff979a6e
Luis Gerardo Barquero Aguilar <LGBARQUEROA@bncrcp.inst.bncr.fi.cr>
2022-04-19 17:58:34 -0600

    Modificacion de la automatizacion de procesarPago

--- cypress/integration/convenios/03-procesarPago.js
+++ cypress/integration/convenios/03-procesarPago.js
@@ -10,14 +10,64 @@ describe('Convenios', () => {
             let xmlString = body;
             let parser = new DOMParser();
             let xml = parser.parseFromString(xmlString, "application/xml");
+            let valorServicio = xml.getElementsByTagName("sn:valorServicio")[0].childNodes[0].nodeValue;
+            let cuentaIBANOrigen = xml.getElementsByTagName("sn:cuentaIBANOrigen")[0].childNodes[0].nodeValue;
+            let numeroFactura = xml.getElementsByTagName("sn:numeroFactura")[0].childNodes[0].nodeValue;
+
+            const num = 1 
+            let incrementoNumeroFactura = parseInt(numeroFactura) + parseInt(num);
             
+            Number.prototype.zfill = function(size) {
+                var numb = String(this);
+                while (numb.length < (size || 2)) {
+                  numb = '0' + numb;
+                }
+                return numb;
+            }
+
+            body = xmlString.replace(numeroFactura, incrementoNumeroFactura.zfill(20));
+
             cy.postMethod(path, body).then((response) => {
                 let xmlString = response.body;
                 let parser = new DOMParser();
                 let xml = parser.parseFromString(xmlString, "application/xml");
 
-                console.log(xml)
-                
+                const msj = 'El recibo ya habia sido cancelado'
+                let index = xmlString.search(msj)
+
+                if(index >= 0){
+                    cy.writeFile('cypress/fixtures/convenios/procesarPago.xml', body)    
+                }
+
+                cy.convertXmlToJson(xml).then((json) =>{
+                    expect(json).not.to.be.empty
+                    
+                    if(json["soapenv:Envelope"]["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:recibos"]["sn:recibo"]["sn:resultado"]["esq:codigo"] == 0){
+                            
+                        let xmlComprobante = json["soapenv:Envelope"]["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:recibos"]["sn:recibo"]["sn:xmlComprobante"]
+                        let parser = new DOMParser();
+                        let xml = parser.parseFromString(xmlComprobante, "application/xml");
+
+                        cy.writeFile('cypress/fixtures/convenios/procesarPago.xml', body)    
+                        
+                        cy.convertToJson(xml).then((json) =>{
+                            expect(json).not.to.be.empty
+                            expect(json["RubroEncabezado"][3]["strValorRubro"]).equals(valorServicio)
+                            expect(json["RubroEncabezado"][8]["strValorRubro"]).to.not.equals(0)
+                            expect(json["RubroEncabezado"][9]["strValorRubro"]).equals(cuentaIBANOrigen)
+                        })
+
+                        expect(json["soapenv:Envelope"]["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:recibos"]["sn:recibo"]["sn:resultado"]["esq:codigo"]).equals('0')
+                        expect(json["soapenv:Envelope"]["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:recibos"]["sn:recibo"]["sn:resultado"]["esq:mensaje"]).equals('Transacción Procesada')
+                    }
+                    else {
+                        assert.fail('Código ' + json["soapenv:Envelope"]["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:recibos"]["sn:recibo"]["sn:resultado"]["esq:codigo"] 
+                            + ', ' + json["soapenv:Envelope"]["env:Body"]["sn:respuesta"]["sn:cuerpo"]["sn:salidaServicio"]["sn:recibos"]["sn:recibo"]["sn:resultado"]["esq:mensaje"])
+                    }
+                })
+            
+                expect(response.status).eq(200)
+                assert.equal(response.headers['content-type'], 'text/xml; charset=utf-8')
             })
         })
     })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -54,7 +54,6 @@ Cypress.Commands.add('convertToJson', (xml) =>{
     return json
 })
 
-
 // Generic function
 function convertirXmlEnObjeto(xml) {
     
@@ -115,6 +114,40 @@ function convertirXmlEnObjeto(xml) {
         }
     
         return object;  
+}
+
+Cypress.Commands.add('convertXmlToJson', (xml) =>{
+    var json = xmlToJson(xml)
+    return json
+})
+
+function xmlToJson(xml) {
+    try {
+      var obj = {};
+      if (xml.children.length > 0) {
+        for (var i = 0; i < xml.children.length; i++) {
+          var item = xml.children.item(i);
+          var nodeName = item.nodeName;
+  
+          if (typeof (obj[nodeName]) == "undefined") {
+            obj[nodeName] = xmlToJson(item);
+          } else {
+            if (typeof (obj[nodeName].Push) == "undefined") {
+              var old = obj[nodeName];
+  
+              obj[nodeName] = [];
+              obj[nodeName].Push(old);
+            }
+            obj[nodeName].Push(xmlToJson(item));
+          }
+        }
+      } else {
+        obj = xml.textContent;
+      }
+      return obj;
+    } catch (e) {
+        console.log(e.message);
+    }
 }
 
 Cypress.Commands.add('sqlServerDB', (query) => {
